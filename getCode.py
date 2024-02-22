9a0bc6ecc3ea4521e591a4d3ea02903c72d5d625
kensho0101 <103886879+taroa36912@users.noreply.github.com>
2024-02-22 20:50:54 +0900

    Update getCode.py

--- getCode.py
+++ getCode.py
@@ -1,15 +1,11 @@
-import win32com.client, re
+import win32com.client, re, requests, pythoncom
+from flask import Flask, request, Response, jsonify
+from pyngrok import ngrok, conf
+from flask_cors import CORS
 
-def GetCode():
-    outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-    account = outlook.Folders[0]
-    folder = account.Folders[6]
-    mails = folder.Items
-    for mail in mails:
-        if mail.Unread:
-            mail.Unread = False
-            return mail.body
-    return
+app = Flask(__name__)
+CORS(app)
+
 
 def extract_digits(text):
     # 正規表現を使用して数字のみを抽出します
@@ -22,16 +18,31 @@ def extract_digits(text):
         return None
 
 
+def GetCode():
+    outlook = win32com.client.Dispatch("Outlook.Application",pythoncom.CoInitialize()).GetNamespace("MAPI")
+    account = outlook.Folders[0]
+    folder = account.Folders[6]
+    mails = folder.Items
+    for mail in mails:
+        if mail.Unread:
+            mail.Unread = False
+            return extract_digits(mail.body)
+    return None
 
 
+@app.route('/')
 def main():
-    while True:
+    print("start to get")
+    count = 0
+    while count < 100:  # 5回試行する例として設定
         result = GetCode()
         if result:
-            number = extract_digits(result)
-            print(number)
-            return number
-        
-        
+            print("verification code : ", result)
+            return jsonify({"verification_code": result})
+        count += 1
+    return jsonify({"error": "Verification code not found"})
+
 if __name__ == "__main__":
-    main()
+    public_url = ngrok.connect(5000)
+    print(f"ngrok URL: {public_url}")
+    app.run(port=5000)
